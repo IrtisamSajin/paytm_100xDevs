c6602ac4d5590a5b48206c0a83916aae9c9e6879
Mohammed Irtisam Sajin <86884797+IrtisamSajin@users.noreply.github.com>
2024-05-11 22:05:57 +0600

    Signin Validation

--- backend/routes/signinRoute.js
+++ backend/routes/signinRoute.js
@@ -1,9 +1,34 @@
 const express=require("express");
+const User=require("../db");
+const zod=require("zod");
+const jwt=require("jsonwebtoken");
 
 const router=express.Router();
 
 router.get("/",(req,res) => {
     res.send("Signin Route");
+})
+
+const signinSchema=zod.object({
+    username: zod.string().email(),
+    password: zod.string().min(6)
+})
+
+router.post("/", async (req,res) => {
+    try{
+        signinSchema.parse(req.body);
+        const user= await User.findOne(req.body);
+        if(!user){
+            throw new Error;
+        }
+        const token=jwt.sign(user._id.toString(),process.env.JWT_KEY);
+        res.status(200).json({token});
+    }
+    catch(err){
+        res.status(411).json({
+            message: "Error while logging in"
+        });
+    }
 })
 
 module.exports=router;
