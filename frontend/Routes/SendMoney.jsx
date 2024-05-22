21879d7c8cedda93ee0e1e0aa7c3d7ebcc25f525
Mohammed Irtisam Sajin <irtisam.coding@gmail.com>
2024-05-22 22:48:54 +0600

    Client Routes

---
+++ frontend/Routes/SendMoney.jsx
@@ -0,0 +1,9 @@
+function SendMoney(){
+    return (
+        <>
+            SendMoney
+        </>
+    )
+}
+
+export default SendMoney;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,36 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Dashboard from "../Routes/Dashboard";
+import SendMoney from "../Routes/SendMoney";
+import Signin from "../Routes/Signin";
+import Signup from "../Routes/Signup";
+
+const router=createBrowserRouter([
+  {
+    path: "/signup",
+    element: <Signup />
+  },
+  {
+    path: "/signin",
+    element: <Signin />
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />
+  },
+  {
+    path: "/send",
+    element: <SendMoney />
+  },
+]);
 
 function App() {
-
   return (
-    <div>
-        Hello world
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   )
 }
 
